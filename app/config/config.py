b0e82e66c10e4d1d0f1f388566af95ac4296b5c7
VladislavKiseliov <vladislavkiseliov@yandex.ru>
2025-10-09 16:38:19 +0400

    Added : New text_splitter

--- app/config/config.py
+++ app/config/config.py
@@ -2,6 +2,8 @@ from pathlib import Path
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from dotenv import load_dotenv
+from langchain_experimental.text_splitter import SemanticChunker
+
 load_dotenv()
 
 # Пути и директории (с возможностью переопределить через .env)
@@ -20,10 +22,24 @@ MAX_RESULTS = 5
 EMBEDDING_MODEL_NAME = "BAAI/bge-m3"
 LLM_MODEL_NAME = 'models/gemini-2.5-flash'
 
-# Настройки текстового разделителя
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=CHUNK_SIZE,
-    chunk_overlap=CHUNK_OVERLAP,
-    separators=[r"\n\n\n", r"\n\n", r"\n", r".\s", r"!\s", r"?\s", r"\s", ""],
-    length_function=len
-)
+# # Настройки текстового разделителя
+# text_splitter = RecursiveCharacterTextSplitter(
+#     chunk_size=CHUNK_SIZE,
+#     chunk_overlap=CHUNK_OVERLAP,
+#     separators=[r"\n\n\n", r"\n\n", r"\n", r".\s", r"!\s", r"?\s", r"\s", ""],
+#     length_function=len
+# )
+
+
+from langchain_huggingface import HuggingFaceEmbeddings # Используйте модель из вашего файла
+
+# 1. Инициализация модели эмбеддингов
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2") # Пример
+
+# 2. Инициализация SemanticChunker
+# В качестве разделителя используется 'sentence'
+text_splitter = SemanticChunker(embeddings)
+
+# # 3. Разбиение текста
+# long_text = "Ваш очень длинный текст с разными темами..."
+# semantic_chunks = text_splitter.split_text(long_text)
